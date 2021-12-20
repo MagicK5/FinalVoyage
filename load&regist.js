0e1d201ab6a882d107c250883591d6b6c5eca695
magicK5 <magichorus5@gmail.com>
2021-12-20 09:19:38 +0800

    test changes

---
+++ load&regist.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app =express()
+const mongoose = require('mongoose')
+const ejs =require('ejs')
+mongoose.connect('mongodb://172.21.2.236:27017/190110910834');
+
+const schema={
+    name: String,
+    age:Number,
+    health:String,
+    hobby:String
+}
+const mydata = mongoose.model('cat1s', schema);
+// const kitty = new mydata({ name: 'testZildjian'});
+// kitty.save()
+
+app.use('/',express.static('public'))
+app.get("/input",(req,res)=>{
+    res.send(req.query)
+    console.log(req.query)
+
+    const kitty = new mydata({ name: req.query.first,health: req.query.second});
+    kitty.save()
+    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+        res.send(str)
+    })
+})
+app.listen(10834)
