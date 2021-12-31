6f02b21181102ec54769a4b984411642331c11f5
magicK5 <magichorus5@gmail.com>
2021-12-31 16:16:49 +0800

    install  swig,body-parser,coolies;prepare css,botstrap;

--- main.js
+++ main.js
@@ -2,26 +2,15 @@ const express = require("express")
 const app =express()
 const mongoose = require('mongoose')
 const ejs =require('ejs')
-mongoose.connect('mongodb://172.21.2.236:27017/190110910834');
 
-const schema={
-    name: String,
-    age:Number,
-    health:String,
-    hobby:String
-}
-const mydata = mongoose.model('cat1s', schema);
-// const kitty = new mydata({ name: 'testZildjian'});
-// kitty.save()
-
-app.use('/',express.static('public'))
+app.use('/',express.static('view'))
 app.get("/input",(req,res)=>{
     // res.send(req.query)
     // console.log(req.query)
 
     const kitty = new mydata({ name: req.query.first,health: req.query.second});
     kitty.save()
-    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+    ejs.renderFile("view/result.html",{returnVal:"success"},(err,str)=>{
         res.send(str)
     })
 })
